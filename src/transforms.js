1db0ec609705bd24a27cf73f75d423216dc20cd8
pavittarx <pavittarx@users.noreply.github.com>
2020-05-23 10:40:20 +0530

    fixed image parsing, added paragreaph support

--- src/transforms.js
+++ src/transforms.js
@@ -1,25 +1,30 @@
 export default {
-  delimiter : ()=>{
-    return `<br/>`;    
+  delimiter: () => {
+    return `<br/>`;
   },
 
-  header: (data)=>{
+  header: ({ data }) => {
     return `<h${data.level}> ${data.text} </h${data.level}>`;
   },
 
-  paragraph: (data)=>{
+  paragraph: ({ data }) => {
     return `<p> ${data.text} </p>`;
   },
 
-  list: (data) => {
-    let style = data.style === 'unordered'? 'ul' : 'ol';
-   let  list = data.items.map( i => `<li> ${i} </li>`).reduce((a,c) => a+c, '' );
+  list: ({ data }) => {
+    let style = data.style === "unordered" ? "ul" : "ol";
+    let list = data.items
+      .map((i) => `<li> ${i} </li>`)
+      .reduce((a, c) => a + c, "");
     return `<${style}> ${list} </${style}`;
   },
 
-  image: (data) => {
-    let caption = data.caption? data.caption: 'Image';
-    return `<img src="${data.file.url}" alt="${caption}"`;
-  }
+  image: ({ data }) => {
+    let caption = data.caption ? data.caption : "Image";
+    return `<img src="${data.file.url}" alt="${caption}" />`;
+  },
 
-}
+  paragraph: ({ data }) => {
+    return `<p> ${data.text} </p>`;
+  },
+};
