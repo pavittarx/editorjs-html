1bcc363946503f9469d25e3ad0052c6844bac5eb
Luzefiru <ian.deejay.iv@gmail.com>
2024-12-29 01:38:43 +0800

    fix: handle undefined item.items

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+const prettier = require('eslint-plugin-prettier');
+
+module.exports = [
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      semi: ['warn', 'always'],
+    },
+  },
+];
